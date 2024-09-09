5fffdd40cd812e01ae5bd6c2287711b56103360d
lorenr-code <52045256+lorenr-code@users.noreply.github.com>
2024-09-09 20:29:28 -0300

    Create routes.tsx

---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom"
+import Home from "./views/Home"
+
+const RoutesConfig = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />
+        }
+    ])
+
+export default RoutesConfig
