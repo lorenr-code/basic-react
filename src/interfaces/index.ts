38ab4a2e33d393b5248a65c36b185402b996b8f6
lorenr-code <52045256+lorenr-code@users.noreply.github.com>
2024-09-09 20:24:03 -0300

    Create index.ts

---
+++ src/interfaces/index.ts
@@ -0,0 +1,41 @@
+export interface IDados<T> {
+  resultado: T;
+  qtdRegistros: number;
+  mensagem?: string;
+  codNotificacao?: string;
+}
+
+export interface IUsuario {
+  Id: number;
+  IdPessoa: number;
+  Login: string;
+  Nome: string;
+  Matricula: string;
+  Cargo: string;
+  Foto: string;
+  IdArea: number;
+  NomeArea: string;
+  NomeDiretoria: string;
+  CodigoSituacao: string;
+  Situacao: string;
+  Email: string;
+  Documento: string;
+  TipoGestor: string;
+  SegredoMFA: string;
+  ValidadeMFA: Date;
+  SituacaoMFA: string;
+  TipoUsuario: number;
+}
+
+export interface IInfo {
+  NomeSistema: string;
+  LinkSistema: string;
+  ImagemSistema: string;
+  DescricaoSistema: string;
+  Versao: string;
+  VersaoFramework: string;
+  CodigoModulo: string;
+  ConexaoTibero: string;
+}
+
+export interface IMenu {}
